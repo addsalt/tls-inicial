0e6888b348a8b71d7b9b446d1f3d66d599c98877
Daniela <just@addsalt.ca>
2014-12-07 21:51:28 -0500

    Navigation Elements Added

--- js/nav.js
+++ js/nav.js
@@ -16,22 +16,3 @@
 		
 		}
 	});
-
-
-// Toggle Search Bar
-
-    var searchBar = document.querySelector('.searchbar');
-
-    document.querySelector('.search-btn').addEventListener('click', function(e){
-        e.preventDefault();
-        
-        if (searchBar.getAttribute('data-state') == 'open'){
-            
-            searchBar.setAttribute('data-state', 'closed');
-            
-        }else{
-            
-            searchBar.setAttribute('data-state', 'open');
-            
-        }
-    });
